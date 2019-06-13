20963415bf35898812f4cd395c39bfa5d882cc7e
Kathy Bradt <kathybradt717@gmail.com>
2019-06-12 21:26:15 -0500

    added data, cycling through questions working

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 console.log('script.js is connected')
 let qNum = 0;
 let scoreRight = 0;
-let scoreWrong =0;
+let scoreWrong = 0;
 
 function startQuiz(){
   $('#main-box').on('click', '.nextButton', event=>{
     $('#main-box').empty();
-    $('.questionNumber').text(1);
+    qNum++;
+    $('.questionNumber').text(qNum);
     renderQuestion();
   })
 };
@@ -14,29 +15,41 @@ function startQuiz(){
 function renderQuestion(){
   if (qNum < quizData.length){
     console.log('adding question info');
-    $('#main-box').html('<form><label>'+quizData[qNum].question+'</label><br><img src="'+quizData[qNum].img+'"><alt>'+quizData[qNum].alt+'</alt><input type="radio"  value="answer1" >'+quizData[qNum].answer1+'<input type="radio"  value="answer2">'+quizData[qNum].answer2+'<br><input type="radio"  value="answer3">'+quizData[qNum].answer3+'<input type="radio"  value="answer4">'+quizData[qNum].answer4+'<br><br><br><input type="submit" value="Submit"></form>');
+    $('#main-box').html('<form><label>'+quizData[qNum-1].question+'</label><br><img src="'+quizData[qNum-1].img+'" alt='+quizData[qNum-1].alt+'><br><input type="radio"  value="answer1" >'+quizData[qNum-1].answer1+'<input type="radio"  value="answer2">'+quizData[qNum-1].answer2+'<br><input type="radio"  value="answer3">'+quizData[qNum-1].answer3+'<input type="radio"  value="answer4">'+quizData[qNum-1].answer4+'<br><br><br><input type="submit" value="Submit" class="answerButton"></form>');
 
   } else {
       console.log('ready to give total score');
   }
 };
 
+function scoreTally(){
+    $('.numRight').text(scoreRight);
+    $('.numWrong').text(scoreWrong);
+};
+
 function handleUserAnswer(){
-    $('form').on('submit', function (event) {
-        console.log('submittion accapted');
+    $('#main-box').on('submit', function (event) {
+        event.preventDefault();
         let selected = $('input:checked');
+        console.log (selected);
         let userAnswer = selected.val();
-    if (correctAnswer[qNum]===userAnswer){
-        $('#main-box').html('<label>That`s right!</label><br><button type="button" class="nextButton">Next!</button>')
+    if (correctAnswer[qNum-1]===userAnswer){
+        $('#main-box').html('<label>That`s right!</label><br><button type="button" class="nextButton">Next!</button>');
+        scoreRight++;
+        scoreTally();
     } else {
-        $('#main-box').html('<label>Woops! You better watch more movies</label><br><button type="button" class="nextButton">Next!</button>')
+        $('#main-box').html('<label>Woops! You better watch more movies</label><br><button type="button" class="nextButton">Next!</button>');
+        scoreWrong++;
+        scoreTally();
     }
+
     })
     };
 
 function runQuiz(){
-  startQuiz(); //removes opening text, change questionNumber Pull next question from datastore
-  handleUserAnswer();//accept submit, compare to correct, give rightor wrong, add to score, add to q number;
+    scoreTally();//show current score
+    startQuiz(); //removes opening text, change questionNumber Pull next question from datastore
+    handleUserAnswer();//accept submit, compare to correct, give rightor wrong, add to score, add to q number;
 };
 
 $(runQuiz);
