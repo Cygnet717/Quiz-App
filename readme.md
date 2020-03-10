09ca198a046e1c6d4f18e8135f87bb1febf1102b
Kathy Bradt <kathybradt717@gmail.com>
2020-03-10 11:36:12 -0500

    fixed word overflow problem

--- readme.md
+++ readme.md
@@ -5,11 +5,8 @@ index.html
 main.css
 script.js
 data.js
-
-I found the most challenging part of this app to be actually trying to get the 4 multipul choice answers to stack in 2 colums like I had planned.  I ended up having to put in a lot of <div>'s to make them sort the way I wanted.  I though about making it easier on myself and just letting them be in one column but I went with the challenge.  This turned out to cause a bit of a problem with accessability though because tab wouldn't cycle through all the answer options so I had to work around that by adding tabindex="0" to each answer option.  
+  
 
 tweaking plans:
 add fading in and out for the questions and resluts
 hover over effects for the answer options and buttons
-
-
--- script.js
+++ script.js
@@ -25,7 +25,6 @@ function displayQuestionNumber(){
 
 function startQuiz(){
   $('#main-box').on('click', '.nextButton', event=>{
-    console.log('button clicked');
     $('#main-box').empty();
     qNum++;
     displayQuestionNumber();
@@ -35,20 +34,35 @@ function startQuiz(){
 
 function finalScore(){
   if (scoreRight <=5){
-    $('#main-box').html('<p>Your score was '+scoreRight+'/10</p><p>"Dishonour! Dishonour on your whole family! Make a note of this: Dishonour on you! Dishonour on your cow!"<br>-- Mulan</p><br><br><button type="button" class="restartButton">Take the Quiz again!</button>');  
+    $('#main-box').html(`<p>Your score was ${scoreRight}/10</p><p>"Dishonour! Dishonour on your whole family! Make a note of this: Dishonour on you! Dishonour on your cow!"<br>-- Mulan</p><br><br><button type="button" class="restartButton">Take the Quiz again!</button>`);  
   } else if (scoreRight>5 && scoreRight<7){
-    $('#main-box').html('<p>Your score was '+scoreRight+'/10</p><p>"You don`t understand! I coulda had class. I coulda been a contender. I could`ve been somebody, instead of a bum, which is what I am."<br>-- On the Waterfront</p><br><br><button type="button" class="restartButton">Take the Quiz again!</button>');
+    $('#main-box').html(`<p>Your score was ${scoreRight}/10</p><p>"You don\`t understand! I coulda had class. I coulda been a contender. I could\`ve been somebody, instead of a bum, which is what I am."<br>-- On the Waterfront</p><br><br><button type="button" class="restartButton">Take the Quiz again!</button>`);
   } else if (scoreRight==8 || scoreRight==9){
-    $('#main-box').html('<p>Your score was '+scoreRight+'/10</p><p>"You`re gonna need a bigger boat."<br>-- Jaws</p><br><br><button type="button" class="restartButton">Take the Quiz again!</button>');
+    $('#main-box').html(`<p>Your score was ${scoreRight}/10</p><p>"You\`re gonna need a bigger boat."<br>-- Jaws</p><br><br><button type="button" class="restartButton">Take the Quiz again!</button>`);
   } else {
-    $('#main-box').html('<p>Your score was '+scoreRight+'/10</p><p>"You`re so wise. You`re like a miniature Buddha, covered in hair."<br>-- Ron Burgundy to his dog in "Anchorman: The Legend of Ron Burgundy</p><br><br><button type="button" class="restartButton">Take the Quiz again!</button>');
+    $('#main-box').html(`<p>Your score was ${scoreRight}/10</p><p>"You\`re so wise. You\`re like a miniature Buddha, covered in hair."<br>-- Ron Burgundy to his dog in "Anchorman: The Legend of Ron Burgundy</p><br><br><button type="button" class="restartButton">Take the Quiz again!</button>`);
   }
 };
 
 function renderQuestion(){
   if (qNum <= quizData.length){
-    console.log('adding question info');
-    $('#main-box').html('<legend>'+quizData[qNum-1].question+'</legend><img src="'+quizData[qNum-1].img+'" alt='+quizData[qNum-1].alt+'><div class="answer" role="radiogroup"><label class="choiceLeft" tabindex="0"><input type="radio" role="radio" value="'+quizData[qNum-1].answer1+'" name="answer">'+quizData[qNum-1].answer1+'</label><label class="choiceLeft" tabindex="0"><input type="radio" role="radio" value="'+quizData[qNum-1].answer2+'" name="answer">'+quizData[qNum-1].answer2+'</label><label class="choiceRight" tabindex="0"><input type="radio" role="radio" value="'+quizData[qNum-1].answer3+'" name="answer">'+quizData[qNum-1].answer3+'</label><label class="choiceRight" tabindex="0"><input type="radio" role="radio" value="'+quizData[qNum-1].answer4+'" name="answer">'+quizData[qNum-1].answer4+'</label></div><input type="submit" value="Submit" class="answerButton">');
+    $('#main-box').html(`<legend class='words'>${quizData[qNum-1].question}</legend>
+    <img src="${quizData[qNum-1].img}" alt=${quizData[qNum-1].alt}>
+    <div class="answer" role="radiogroup">
+      <label class="choiceLeft" tabindex="0">
+        <input type="radio" role="radio" value="${quizData[qNum-1].answer1}" name="answer">${quizData[qNum-1].answer1}
+      </label>
+      <label class="choiceLeft" tabindex="0">
+        <input type="radio" role="radio" value="${quizData[qNum-1].answer2}" name="answer">${quizData[qNum-1].answer2}
+      </label>
+      <label class="choiceRight" tabindex="0">
+        <input type="radio" role="radio" value="${quizData[qNum-1].answer3}" name="answer">${quizData[qNum-1].answer3}
+      </label>
+      <label class="choiceRight" tabindex="0">
+        <input type="radio" role="radio" value="${quizData[qNum-1].answer4}" name="answer">${quizData[qNum-1].answer4}
+      </label>
+    </div>
+    <input type="submit" value="Submit" class="answerButton">`);
 
   } else {
       console.log('ready to give total score');
@@ -69,13 +83,13 @@ function handleUserAnswer(){
         let userAnswer = selected.val();
         console.log (userAnswer);
     if (correctAnswer[qNum-1]===userAnswer){
-        $('#main-box').html('<img src="'+moviePosters[qNum-1]+'"<label>That`s right!</label><br><button type="button" class="nextButton">Next!</button>');
+        $('#main-box').html(`<img src="${moviePosters[qNum-1]}"<label>That\`s right!</label><br><button type="button" class="nextButton">Next!</button>`);
         scoreRight++;
         scoreTally();
     } else if (userAnswer==undefined){
         alert('Please select an answer.');
     } else {
-        $('#main-box').html('<img src="'+moviePosters[qNum-1]+'"<label>Woops! You better watch more movies</label><br><button type="button" class="nextButton">Next!</button>');
+        $('#main-box').html(`<img src="${moviePosters[qNum-1]}"<label>Woops! You better watch more movies</label><br><button type="button" class="nextButton">Next!</button>`);
         scoreWrong++;
         scoreTally();
     }
